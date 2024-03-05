e76212d5a45afc7ceefba3d10092dc5a7d60f214
Hamid <Bouayadihamid@gmail.com>
2024-03-05 14:24:33 -0800

    sibling

--- binary_trees.h
+++ binary_trees.h
@@ -29,6 +29,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_root(const binary_tree_t *node);
 int binary_tree_is_leaf(const binary_tree_t *node);
